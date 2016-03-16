f3cc2746d196a5f184f57d351f3755ab343eef6f
Skylar King <sking@crazyawesome.io>
2016-03-16 18:06:20 -0500

    Ininital commits for methods problems.

---
+++ methods.js
@@ -0,0 +1,15 @@
+//Assign "Hello World" to a variable message
+  var world = "Hello World";
+//Assign a different string to a different variable
+  var aString = "What do I put here?";
+//Assign a number to a variable
+  var number = 5054;
+//Use string interpolation'ish to display the number in a string
+  var numAndString = "Do I put " + number + " here?";
+//Make an array of your favorite movies or books or bands. Have at least 4 values.
+  var favMovies = [
+    "Fight Club",
+    "Gladiator",
+    "The Wolf of Wall Street",
+    "The Pursuit of Happiness"
+  ]
